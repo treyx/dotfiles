06cd138a2f435b3f19bd24be9667130f33f56ee5
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-28 22:30:38 -0600

    bit of formatting

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,79 +1,61 @@
-# ------- #
-# aliases #
-# ------- #
+# ----- #
+# shell #
+# ----- #
 
+alias path="echo '$PATH' | tr : '\n'"                             # display path for troubleshooting
 
 # --- #
 # git #
 # --- #
 
 alias gs="git status"
-alias gds="git diff | subl"
-alias gc="git checkout"
-alias ga="git add ."
-alias gb='git branch -av'
-alias gpu="git push --set-upstream origin HEAD"
-alias gcb="git checkout -b"
-alias gclean="git branch --merged master | ag -v '\* master' | xargs -n 1 git branch -d" # removes local branches that have been merged into master
+alias gds="git diff | subl"                                       # open git diff in st3
+alias ga="git add ."                                              # adds all uncommited changes to staging
+alias gb='git branch -av'                                         # show all local and remote branches with last commit
+alias gpu="git push --set-upstream origin HEAD"                   # pushes and sets upstream / thanks @neslom
+alias gcb="git checkout -b"                                       # create and checkout to new branch
+alias gclean="git branch --merged master | ag -v '\* master' \
+              | xargs -n 1 git branch -d"                         # removes local branches that have been merged into master
+alias gcm='git commit -m "'                                       # commit with message
+alias grl="git remote --verbose"                                  # show remotes
+alias gra="git remote add"                                        # add remote
+alias glc="git shortlog --summary --numbered"                     # print contributors by # of commits
+alias gl="git log --all --graph --decorate"                       # detailed log
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset \
+            %an: %s - %Creset %C(yellow)%d%Creset \
+            %Cgreen(%cr)%Creset' --abbrev-commit --date=relative" # pretty log
 
-# # Log (l)
-# print contributors by # of commits
-alias glc="git shortlog --summary --numbered"
-#detailed log
-alias gl="git log --all --graph --decorate"
-#prettylog
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-#shortlog
-alias gls='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
-
-# # Remote (r)
-alias grl="git remote --verbose"
-alias gra="git remote add"
-
-
-alias s="subl"
-alias ss="subl ."
-
+alias gls='git log --topo-order --all --graph \
+           --pretty=format:"${_git_log_oneline_format}"'          # short log
 
 # ---------- #
 # navigation #
 # ---------- #
 
-alias ..="cd .." # go up one directory
+alias ..="cd .."                                                  # go up one directory
+alias todo="subl ~/Documents/todo.txt"                            # to do list
+alias dotf="subl ~/.dotfiles"                                     # manage my dotfiles
+
+alias s="subl"
+alias ss="subl ."
 
 alias tu1="cd ~/turing/module1"
 alias tu2="cd ~/turing/module2"
 alias tu3="cd ~/turing/module3"
 
-# alias l='ls -1A'         # Lists in one column, hidden files.
-# alias ll='ls -lh'        # Lists human readable sizes.
-# alias lr='ll -R'         # Lists human readable sizes, recursively.
-# alias la='ll -A'         # Lists human readable sizes, hidden files.
-# alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-# alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-# alias lk='ll -Sr'        # Lists sorted by size, largest last.
-# alias lt='ll -tr'        # Lists sorted by date, most recent last.
-# alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-# alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-# alias sl='ls'            # I often screw this up.
+# ------------------ #
+# directory contents #
+# ------------------ #
 
-# List directory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
+alias l="ls -lah"                                                 # lists all with hidden files in readable sizes
+alias ll="ls -lh"                                                 # lists human readable sizes
+alias lt="ls -lahctr"                                             # lists sorted by date /most recent last / shows change time
 
 # Resource Usage
 # alias df='df -kh'
-
-alias path="echo '$PATH' | tr : '\n'" # display path for troubleshooting
 
 # ------------- #
 # miscellaneous #
 # ------------- #
 
-alias week='date +%V' # get week number
-
-alias todo="subl ~/Documents/todo.txt"
-
-alias dotf="subl ~/.dotfiles"
+alias week="date +%V"                                             # get week number
