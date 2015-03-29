38b10eb93135573a3a425e45c3edc6b6c0e1b357
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-28 22:03:02 -0600

    added rails_pg function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,11 +1,55 @@
+# ----- #
+# shell #
+# ----- #
+
+
 # Create a new directory and enter it
 function md() {
   mkdir -p "$@" && cd "$@"
 }
 
+# --- #
+# git #
+# --- #
+
 
 # git commit / twitter
-# function gcm {
+# function gcmt {
 #   git commit -m "$1"
 #   ruby -e 'require %(jumpstart_auth);ARGV.join(" ").scan(/.{1,140}/).each { |tweet| JumpstartAuth.twitter.update(tweet)}' $1
 # }
+
+
+# ----- #
+# rails #
+# ----- #
+
+function rails_pg() {
+  rails new $1 -T -B --database=postgresql
+
+  cd $1
+
+  add_rails_gems
+  bundle
+  rails generate rspec:install
+
+  git init
+  git add .
+  git commit -m "initial commit"
+
+  subl .;
+}
+
+function add_rails_gems() {
+
+echo "
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'pry-rails'
+end
+
+" >> Gemfile;
+
+}
