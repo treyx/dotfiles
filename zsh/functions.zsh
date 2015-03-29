de4d070908049d8664c451ae6e9eda79613adbf2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-28 18:48:02 -0600

    added twitter/commit function - need to fix

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,3 +2,10 @@
 function md() {
   mkdir -p "$@" && cd "$@"
 }
+
+
+# git commit / twitter
+# function gcm {
+#   git commit -m "$1"
+#   ruby -e 'require %(jumpstart_auth);ARGV.join(" ").scan(/.{1,140}/).each { |tweet| JumpstartAuth.twitter.update(tweet)}' $1
+# }
