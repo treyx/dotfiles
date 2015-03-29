aeeb68879fcf7868db6feaf6a33abef17cc9f8d0
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-28 18:49:16 -0600

    cleaned up aliases a bit... still needs work

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,24 +2,19 @@
 # aliases #
 # ------- #
 
-alias todo="subl ~/Documents/todo.txt"
 
-alias dotf="subl ~/.dotfiles"
+# --- #
+# git #
+# --- #
 
-
-alias git="hub"
 alias gs="git status"
 alias gds="git diff | subl"
 alias gc="git checkout"
 alias ga="git add ."
 alias gb='git branch -av'
-
 alias gpu="git push --set-upstream origin HEAD"
-
 alias gcb="git checkout -b"
-
-#removes all local branches that have been merged into master
-alias gclean="git branch --merged master | ag -v '\* master' | xargs -n 1 git branch -d"
+alias gclean="git branch --merged master | ag -v '\* master' | xargs -n 1 git branch -d" # removes local branches that have been merged into master
 
 # # Log (l)
 # print contributors by # of commits
@@ -36,20 +31,16 @@ alias grl="git remote --verbose"
 alias gra="git remote add"
 
 
-# git commit / twitter
-# function gcm {
-#   git commit -m "$1"
-#   ruby -e 'require %(jumpstart_auth);ARGV.join(" ").scan(/.{1,140}/).each { |tweet| JumpstartAuth.twitter.update(tweet)}' $1
-# }
-
-
 alias s="subl"
 alias ss="subl ."
 
 
-##
+# ---------- #
 # navigation #
-alias ..="cd .."
+# ---------- #
+
+alias ..="cd .." # go up one directory
+
 alias tu1="cd ~/turing/module1"
 alias tu2="cd ~/turing/module2"
 alias tu3="cd ~/turing/module3"
@@ -66,17 +57,23 @@ alias tu3="cd ~/turing/module3"
 # alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 # alias sl='ls'            # I often screw this up.
 
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
 
 # Resource Usage
 # alias df='df -kh'
 
-#display path for troubleshooting
-alias path="echo '$PATH' | tr : '\n'"
-
+alias path="echo '$PATH' | tr : '\n'" # display path for troubleshooting
 
 # ------------- #
 # miscellaneous #
 # ------------- #
 
-# Get week number #
-alias week='date +%V'
+alias week='date +%V' # get week number
+
+alias todo="subl ~/Documents/todo.txt"
+
+alias dotf="subl ~/.dotfiles"
