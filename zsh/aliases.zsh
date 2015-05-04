8479a1e906669f1f541f31a700c44aef8dbe8ac8
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-03 23:10:59 -0600

    added a few more aliases and some cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,13 +2,23 @@
 # shell #
 # ----- #
 
-alias path="echo '$PATH' | tr : '\n'"                         # display path for troubleshooting
-alias disk="df -h ~"                                          # show disk space
-alias psr="ps aux | ag ruby"                                  # show ruby processes
-alias cl="clear"                                              # simple clear
-alias sniff="sudo ngrep -W byline port 80"
-alias tre="tree -aC -I '.git' --dirsfirst "$@" | less -FRNX;" # better tree
+alias s="subl"
+alias ss="subl ."
+alias cl="clear"
+alias path="echo '$PATH' | tr : '\n'"   # display path for troubleshooting
+alias disk="df -h ~"                    # show disk space
+alias psr="ps aux | ag ruby"            # show ruby processes
+alias week="date +%V"                   # get week number
 
+alias sniff="sudo ngrep -W byline port 80"
+alias brewu="brew update && brew upgrade && brew cleanup && brew prune && brew doctor"
+
+# better tree
+alias tre="tree -aC -I '.git' --dirsfirst "$@" | less -FRNX;"
+
+# Empty the Trash on all mounted volumes and the main HDD
+# Also, clear Apple’s System Logs to improve shell startup speed
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
 
 # ----- #
 # rails #
@@ -24,36 +34,32 @@ alias rdm="bundle exec rake db:migrate"
 
 alias gd="git diff"
 alias gs="git status"
-alias gpr="hub pull-request"                                      # create a pull request
-alias gds="git diff | subl"                                       # open git diff in st3
-alias ga="git add"                                                # adds all uncommited changes to staging
-alias gb="git branch -av"                                         # show all local and remote branches with last commit
-alias gpu="git push --set-upstream origin HEAD"                   # pushes and sets upstream / thanks @neslom
-alias gcb="git checkout -b"                                       # create and checkout to new branch
-alias gclean="git branch --merged master | ag -v '\* master' \
-              | xargs -n 1 git branch -d"                         # removes local branches that have been merged into master
-alias grv="git remote -v"                                         # show remotes
-alias gra="git remote add"                                        # add remote
-alias grx="git remote rm"                                         # remove remote
-alias glc="git shortlog --summary --numbered"                     # print contributors by # of commits
-alias gl="git log --all --graph --decorate"                       # detailed log
-alias gbr="git browse"                                            # open repo on github
+alias gpr="hub pull-request"                     # create a pull request
+alias gds="git diff | subl"                      # open git diff in st3
+alias ga="git add"                               # adds all uncommited changes to staging
+alias gb="git branch -av"                        # show all local and remote branches with last commit
+alias gbr="git browse"                           # open repo on github
+alias gpu="git push --set-upstream origin HEAD"  # pushes and sets upstream
+alias gcb="git checkout -b"                      # create and checkout to new branch
+alias grv="git remote -v"                        # show remotes
+alias gra="git remote add"                       # add remote
+alias grx="git remote rm"                        # remove remote
+alias glc="git shortlog --summary --numbered"    # print contributors by # of commits
+alias gl="git log --all --graph --decorate"      # detailed log
 
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset \
-            %an: %s - %Creset %C(yellow)%d%Creset \
-            %Cgreen(%cr)%Creset' --abbrev-commit --date=relative" # pretty log
+# pretty log
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
+# removes local branches that have been merged into master
+alias gclean="git branch --merged master | ag -v '\* master' \ | xargs -n 1 git branch -d"
 
 # ---------- #
 # navigation #
 # ---------- #
 
-alias ..="cd .."                       # go up one directory
-alias todo="subl ~/Documents/todo.txt" # to do list
-alias dotf="subl ~/.dotfiles"          # manage my dotfiles
-
-alias s="subl"
-alias ss="subl ."
-
+alias ..="cd .."
+alias todo="subl ~/Documents/todo.txt"
+alias dotf="subl ~/.dotfiles"
 alias tu1="cd ~/turing/module1"
 alias tu2="cd ~/turing/module2"
 alias tu3="cd ~/turing/module3"
@@ -66,10 +72,3 @@ alias hh="cd ~/turing/module3/hyper_hub"
 alias l="ls -lah"      # lists all with hidden files in readable sizes
 alias ll="ls -lh"      # lists human readable sizes
 alias lt="ls -lahctr"  # lists sorted by date /most recent last / shows change time
-
-# ------------- #
-# miscellaneous #
-# ------------- #
-
-alias brewu="brew update && brew upgrade && brew cleanup && brew prune && brew doctor"
-alias week="date +%V" # get week number
