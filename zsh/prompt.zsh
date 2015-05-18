044aef91310db51b680730874c5e4c0a0a2b90b1
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-17 22:21:19 -0600

    not using anymore

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,6 @@
 ZSH_THEME_GIT_PROMPT_PREFIX="λ %{$fg[blue]%}git %{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 
-# ZSH_THEME_GIT_PROMPT_CLEAN=
-# ZSH_THEME_GIT_PROMPT_DIRTY=
 
 if which rbenv-prompt &> /dev/null; then
   rbenv='%{$fg[green]%}‹$(rvm-prompt i v g)›%{$reset_color%}'
